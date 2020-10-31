7899d482ddc65c0fbd700886a60a0d6f49662c0b
Valentin D <vavadu07@gmail.com>
2020-10-31 17:22:46 +0100

    Mise en place d'une animation sur les titres

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -3,14 +3,16 @@ import { Col } from 'reactstrap';
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faReact } from '@fortawesome/free-brands-svg-icons'
+import {Animated} from "react-animated-css";
 
 function Presentation() {
     return (
         <Col>
-            <h1 style={{textAlign: "center"}}>Valentin Duffet</h1>
-            <h3 style={{textAlign: "center"}}>Développeur web React JS <FontAwesomeIcon icon={faReact} style={{color: "#0984e3"}}/></h3>
+            <Animated animationIn="slideInUp" animationInDuration="1500" isVisible={true}>
+                <h1 style={{textAlign: "center"}}>Valentin Duffet</h1>
+                <h3 style={{textAlign: "center"}}>Développeur web React JS <FontAwesomeIcon icon={faReact} style={{color: "#0984e3"}}/></h3>
+            </Animated>   
         </Col> 
-    
     );
   }
   
