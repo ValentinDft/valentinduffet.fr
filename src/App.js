e07609b03de9b25f799edc8ebcc1f27fe23cd9b2
Valentin D <vavadu07@gmail.com>
2020-11-01 16:22:59 +0100

    ajout animation scroll

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import NavbarTop from'./components/Navbar';
 import Presentation from'./components/Presentation';
 import Education from'./components/Education';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Container} from 'reactstrap';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       <NavbarTop/>
 
       <Presentation/>
+
+    <div data-aos="flip-up">
+      <Education/>
+    </div>
+      
         
        
       
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Col, CardImg, Row, Container } from 'reactstrap';
+import { Col, Row, Container } from 'reactstrap';
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
-import {Animated} from "react-animated-css";
-import imgCard from '../raw.jpg';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+
 
 function Education() {
     return (
-        <Container fluid={true}>
+        <Container fluid={true} style={{backgroundColor: "#c23616", marginTop: "5%"}}>
             <Row>
                 <Col>
-                  
-                  
+                    <h2 style={{textAlign: "center", marginTop: "1%", marginBottom: "1%"}}>EDUCATION <FontAwesomeIcon icon={faGraduationCap} style={{textAlign: "center", fontSize: "35px"}}></FontAwesomeIcon></h2>
+                    <hr style={{borderTop: "8px solid #636e72", borderRadius: "5px", width: "18%", marginBottom: "5%"}}></hr>
+                    
                 </Col>
             </Row>
         </Container>
