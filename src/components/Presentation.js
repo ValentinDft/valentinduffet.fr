4db7b555e4ab7b11a4045066aff517c75ec33f4e
Valentin D <vavadu07@gmail.com>
2020-11-05 23:20:54 +0100

    Update Presentation.js

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -20,8 +20,8 @@ function Presentation() {
                         <h2 style={{textAlign: "center", marginTop: "5%", marginBottom: "3%"}}>PRESENTATION</h2>
                         <hr style={{borderTop: "8px solid #c23616", borderRadius: "5px", width: "18%", marginBottom: "5%"}}></hr>
                         <h4 style={{marginTop: "3%", marginBottom: "10%", textAlign: "justify"}}>
-                            Jeune développeur, passioné par les technologies du web. 
-                            J'ai appris à coder dés le lycée par simple curiosité, pour savoir comment était fait un site web. J'ai vraiment adoré ça. <br></br>
+                            Jeune développeur, passionné par les technologies du web. 
+                            J'ai appris à coder dés le lycée par simple curiosité, pour savoir comment était construit un site web. J'ai vraiment adoré ça. <br></br>
                             Par la suite j'ai effectué un <span style={{color: "#c23616"}}>BTS SNIR (Systèmes Numériques informatique et Réseaux)</span> que j'ai obtenus. <br></br>
                             Et pour solidifier mes connaissances et mes compétences dans le développement web, j'ai effectué une 
                             <span style={{color: "#c23616"}}> formation de développeur web fullstack Javascript à La Capsule.</span></h4>
