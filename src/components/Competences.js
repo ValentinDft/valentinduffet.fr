d7ef1d0350587ad95db4aaf33a1ad1692374f9fc
Valentin D <vavadu07@gmail.com>
2020-11-11 16:23:38 +0100

    Ajout du block competence back-end + contenu

--- src/components/Competences.js
+++ src/components/Competences.js
@@ -4,6 +4,7 @@ import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
 import logoFront from "../front.png";
+import logoBack from "../back.png";
 
 
 function Competences() {
@@ -26,6 +27,20 @@ function Competences() {
                         <h5>Html/Css & Javascript</h5> <br/>
                         <h4>Frameworks et outils: </h4>
                         <h5>React JS, Bootstrap, Ant-design</h5>
+                    </Col> 
+                </Row>
+            </Col>
+
+            <Col className="block-competences" style={{marginTop: "15px"}}>
+                <Row>
+                    <Col sm="12" md="6" lg="9" style={{backgroundColor: "#f5f6fa", borderRadius: "5%", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                        <h4>Les languages : </h4>
+                        <h5>Javascript & php</h5> <br/>
+                        <h4>Frameworks et bdd: </h4>
+                        <h5>Node JS, Express, MongoDB</h5>
+                    </Col> 
+                    <Col sm="12" md="6" lg="3">
+                        <CardImg src={logoBack} alt="Front-end" style={{borderRadius: "5%", width: "100%", height: "100%"}}/>
                     </Col> 
                 </Row>
             </Col>
