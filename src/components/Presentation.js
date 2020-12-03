022cd658a169b7cb7d952e5d8481fed54627c0cf
Valentin D <vavadu07@gmail.com>
2020-12-03 15:46:11 +0100

    Ajout de la vague dans présentation + responsive de la vague

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -25,6 +25,11 @@ function Presentation() {
                     </div>
                 </Col>
             </Row>
+            <div className="waves-bottom-presentation">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path fill="white" d="M0,32L60,32C120,32,240,32,360,64C480,96,600,160,720,165.3C840,171,960,117,1080,85.3C1200,53,1320,43,1380,37.3L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+                </svg>
+            </div>
         </Container>
         
         
