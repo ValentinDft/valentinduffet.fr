5a755ceb95f42b574ba199e8b9d1c6b38640dbff
Valentin D <vavadu07@gmail.com>
2020-12-18 00:58:17 +0100

    Ajout de la fonction envoie de l'email au clique sur envoyer

--- reactapp/src/components/Contact.js
+++ reactapp/src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Col, Row, Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,6 +9,35 @@ import "aos/dist/aos.css";
     
 
 function Contact() {
+    const [nom, setNom] = useState("");
+    console.log('nom:', nom)
+    const [email, setEmail] = useState("");
+    console.log('email:', email)
+    const [message, setMessage] = useState("");
+    console.log('message:', message)
+
+    let clickEnvoyer = async () => {
+        let requete = await fetch('/email', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: `nom=${nom}&email=${email}&message=${message}`,
+        });
+        let response = await requete.json();
+
+        console.log('response:', response);
+
+        // if (response) {
+        //     setSuccessMessage(
+        //         <Alert color='success'>
+        //             {' '}
+        //             Success, check your email : {emailSouscription}
+        //         </Alert>
+        //     );
+        // } else {
+        //     setSuccessMessage(<Alert color='danger'> Error </Alert>);
+        // }
+    }
+
     return (
         <Container style={{marginTop: "5%"}}>
             <h1 style={{textAlign: "center", marginBottom: "1%", color: "#dff9fb"}}>CONTACT</h1>
@@ -18,17 +47,17 @@ function Contact() {
                     <Form style={{width:"50%", display: "flex", flexDirection: "column", justifyContent: "center"}}>
                         <FormGroup>
                             <Label style={{color: "white"}}>Nom et Prénom : </Label>
-                            <Input type="text" name="nom" placeholder="Saisir votre nom et votre prénom" required/>
+                            <Input type="text" name="nom" placeholder="Saisir votre nom et votre prénom" onChange={(e) => setNom(e.target.value)} required/>
                         </FormGroup>
                         <FormGroup>
                             <Label style={{color: "white"}}>Email : </Label>
-                            <Input type="email" name="email" placeholder="Saisir votre email@" required/>
+                            <Input type="email" name="email" placeholder="Saisir votre email@" required onChange={(e) => setEmail(e.target.value)}/>
                         </FormGroup>
                         <FormGroup style={{marginBottom:"6%"}}>
                             <Label style={{color: "white"}}>Message : </Label>
-                            <Input type="textarea" name="message" placeholder="Saisir votre message" required/>
+                            <Input type="textarea" name="message" placeholder="Saisir votre message" required onChange={(e) => setMessage(e.target.value)}/>
                         </FormGroup>
-                        <Button className="button-envoyer">Envoyer</Button>
+                        <Button className="button-envoyer" onClick={()=>clickEnvoyer()}>Envoyer</Button>
                     </Form>
                 </Col>
             </Row>
