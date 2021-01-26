57159abf28a34568a3dd99247ca9ffbcec7b354c
Valentin Duffet <49043842+ValentinDft@users.noreply.github.com>
2021-01-26 15:20:42 +0100

    sup material ui

--- reactapp/src/components/Portfolio.js
+++ reactapp/src/components/Portfolio.js
@@ -15,89 +15,10 @@ import { faBootstrap } from '@fortawesome/free-brands-svg-icons'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
 import { faReact } from '@fortawesome/free-brands-svg-icons'
 
-// Import Material UI
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-// -------------------------------------- Fonction pour le tab material UI ----------------------------------------------
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`full-width-tabpanel-${index}`}
-        aria-labelledby={`full-width-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box p={3}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-      id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
-    };
-}
-
-// -----------------constante de style du tab --------------------
-const useStyles = makeStyles((theme) => ({
-    root: {
-      backgroundColor: "theme.palette.background.paper",
-      width: 300,
-    },
-    indicator: {
-        backgroundColor: "#c23616",
-        height: "2px",
-        top: "45px"
-    },
-    flexContainer: {
-        height: "45px",
-        background: "#f5f6fa"
-    },
-    tab: {
-        '& .MuiBox-root': {
-            padding: '0px',
-        },
-    },
-}));
-
-
 function Portfolio() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    const handleChangeIndex = (index) => {
-        setValue(index);
-    };
-
     const textIconGitHub = <span>Github du projet</span>;
     const textIconSite = <span>Site web</span>
 
